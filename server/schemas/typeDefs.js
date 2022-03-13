2c127b0f7dd2b74c745661e4b91d3f89ab4df336
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-03-12 20:15:27 -0500

    typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+    type user{
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book{
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth{
+        token: ID!
+        user: User
+    }
+
+    type Query{
+        me: user
+    }
+
+    type Mutation{
+        login(email: String!, password: String!): Auth 
+        addUser(username: String!, email: String!, password: String!): Auth
+        savedBooks(authors: [String], description: String!, bookId: String!, image: String, link: String, title: String! ): User
+        removeBook(bookId:String!): User
+    }
+`;
+
+module.export = typeDefs;
